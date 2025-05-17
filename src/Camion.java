02b5e7faa834d1378c36d8be6ea8fa8d8685ec48
Eva Otero Calleiras <evacalleiras@gmail.com>
2025-05-17 23:25:46 +0200

    Añadir propiedades y atributos a Camion

--- src/Camion.java
+++ src/Camion.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+public class Camion implements TransportadorMercancias{
+    private int capacidadeCarga;
+    private int cargaActual;
+    private static final int UNIDADCARGA = 30;
+    private int conductor;
 
-public class Camion {
+    public Camion(int capacidadeCarga, int cargaActual, int conductor) {
+        this.capacidadeCarga = capacidadeCarga;
+        this.cargaActual = cargaActual;
+        this.conductor = conductor;
+    }
+
+    public void mantemento(){
+        System.out.println("Llevando el camión al taller");
+    }
+
+    @Override
+    public double cargar() {
+        double cargar = UNIDADCARGA + 1;
+        return  cargar;
+    }
+
+    @Override
+    public double descargar() {
+        double descargar = UNIDADCARGA - 1;
+        return descargar;
+    }
 }
