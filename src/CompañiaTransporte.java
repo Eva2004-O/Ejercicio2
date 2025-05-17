8b0ebf07184ddb6af7c39602d505d16381b9730b
Eva Otero Calleiras <evacalleiras@gmail.com>
2025-05-17 23:41:05 +0200

    añadir propiedades y métodos a CompañiaTransporte

--- src/CompañiaTransporte.java
+++ src/CompañiaTransporte.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class CompañiaTransporte {
+    private String nome;
+    private ArrayList<Vehiculo> vehiculos;
+
+    public CompañiaTransporte(String nome) {
+        this.nome = nome;
+        vehiculos = new ArrayList<>();
+    }
+
+    public ArrayList<Vehiculo> getVehiculos() {
+        return vehiculos;
+    }
+
+    public void agregarVehiculo(Vehiculo vehiculo){
+        vehiculos.add(vehiculo);
+    }
+
+    public void eliminarVehiculo(int numeroSerie){
+        Iterator<Vehiculo> iterator = vehiculos.iterator();
+        while (iterator.hasNext()){
+            Vehiculo vehiculo = iterator.next();
+            if (vehiculo.numeroSerie == numeroSerie){
+                iterator.remove();
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "CompañiaTransporte{" +
+                "nome='" + nome + '\'' +
+                ", vehiculos=" + vehiculos +
+                '}';
+    }
 }
