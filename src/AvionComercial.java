001355067b3207d2c877c89dad54098d8f0527fe
Eva Otero Calleiras <evacalleiras@gmail.com>
2025-05-17 22:09:50 +0200

    crear objeto avionComercial

--- src/AvionComercial.java
+++ src/AvionComercial.java
@@ -15,8 +15,24 @@ public class AvionComercial extends Avion {
         this.nunPasajeros = nunPasajeros;
         tripulacion = new HashMap<>();
     }
+
+    public HashMap<String, String> getTripulacion() {
+        return tripulacion;
+    }
+
     public void asignarTripulacion(String cargo, String nome){
         tripulacion.put(cargo, nome);
     }
 
+    @Override
+    public String toString() {
+        return "AvionComercial{" +
+                "nunPasajeros=" + nunPasajeros +
+                ", consumo=" + consumo +
+                ", modelo='" + modelo + '\'' +
+                ", numeroSerie=" + numeroSerie +
+                ", tipoCombustible=" + tipoCombustible +
+                ", cantidadCombustible=" + cantidadCombustible +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,11 @@ public class Main {
     public static void main(String[] args) {
         Avion a1 = new Avion("FlyEmirates", 23, 1000);
         Avion a2 = new Avion("Iberia", 54, TipoCombustible.DIESEL, 3040);
+        AvionComercial aC1 = new AvionComercial("Airbus", 4546, TipoCombustible.JET_A1, 10023, 100);
         System.out.println(a1);
         System.out.println(a1.maximaDistanciaPercorrer());
+        aC1.asignarTripulacion("Piloto", "Manuel Fernandez");
+
 
     }
 }
